0cd9002a78dcee017f32ad39f96dea08b35e7bdd
sblack13 <88597164+sblack13@users.noreply.github.com>
2021-08-13 19:53:00 -0400

    initial commit

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
-
     // Create Dino Constructor
-
+    function Dino(species, weight, height, diet, where, when, fact) {
+        this.species = species
+        this.weight = weight 
+        this.height = height
+        this.diet = diet
+        this.where = where
+        this.when = when
+        this.fact = fact
+    }
 
     // Create Dino Objects
-
+    fetch('dino.json')
+    .then(response => response.json())
+    .then(data => console.log(data));
+  
 
     // Create Human Object
+    
 
     // Use IIFE to get human data from form
 
